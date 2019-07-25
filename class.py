eabd73bd4fa94912cc91e2ba176d41c791601a2c
SamyagJ <53125663+SamyagJ@users.noreply.github.com>
2019-07-25 11:53:15 -0700

    Add files via upload

---
+++ class.py
@@ -0,0 +1,31 @@
+import pygame
+
+class player_look:
+    def __init__(self):
+        self.rect.
+
+class player:
+    def __init__(self):
+        self.pygame.name='bolt'
+        self.color=pygame.Color(0,0,225)
+    def colorChange(self):
+        self.pygame.color=(255,0,0)
+    def move(self, movement):
+        if movement is 1:
+            self.player.move_ip(-2,0)
+        if movement is 2:
+            self.player.move_ip(2,0)
+        if movement is 3:
+            self.player.move_ip(0,-2)
+        if movement is 4:
+            self.player.move_ip(2,0)
+
+        def handleCollision():
+            if self.box.rect.colliderect(rect):
+                self.box.colorChange
+
+
+
+
+
+
